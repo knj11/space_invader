689c308e16bda4c1e88c45282aafe6c4206edd31
Koby Johnson <koby.johnson@powertraincontrol.com>
2023-12-21 09:03:31 -0500

    debug: attaching log function to gl debug output

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,29 @@
 #include <stdio.h>
 #include <iostream>
 
+#define GL_ERROR_CASE(glerror)\
+    case glerror: snprintf(error, sizeof(error), "%s", #glerror)
+
+inline void gl_debug(const char *file, int line) {
+    GLenum err;
+    while((err = glGetError()) != GL_NO_ERROR){
+        char error[128];
+
+        switch(err) {
+            GL_ERROR_CASE(GL_INVALID_ENUM); break;
+            GL_ERROR_CASE(GL_INVALID_VALUE); break;
+            GL_ERROR_CASE(GL_INVALID_OPERATION); break;
+            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION); break;
+            GL_ERROR_CASE(GL_OUT_OF_MEMORY); break;
+            default: snprintf(error, sizeof(error), "%s", "UNKNOWN_ERROR"); break;
+        }
+
+        fprintf(stderr, "%s - %s: %d\n", error, file, line);
+    }
+}
+
+#undef GL_ERROR_CASE
+
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
@@ -59,6 +82,8 @@ int main()
                 glfwTerminate();
                 return -1;
         }    
+
+        gl_debug(__FILE__, __LINE__);
 
         // render loop
         // -----------
