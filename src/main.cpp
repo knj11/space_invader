2536ad204d17ae523c5490030edf2676465ccd6a
Koby Johnson <koby.johnson@powertraincontrol.com>
2023-12-21 13:33:06 -0500

    sprite: implementing draw_sprite
    loops through the sprite buffer and determines if it should color the pixel based on screen location and filled in 1's in the sprites buffer

--- src/main.cpp
+++ src/main.cpp
@@ -73,6 +73,21 @@ void buffer_clear(Buffer* buffer, uint32_t color)
     }
 }
 
+void buffer_draw_sprite(Buffer* buffer, const Sprite& sprite, size_t x, size_t y, uint32_t color)
+{
+    for(size_t xi = 0; xi < sprite.width; ++xi)
+    {
+        for(size_t yi = 0; yi < sprite.height; ++yi)
+        {
+            if(sprite.data[yi * sprite.width + xi] &&
+               (sprite.height - 1 + y - yi) < buffer->height &&
+               (x + xi) < buffer->width)
+            {
+                buffer->data[(sprite.height - 1 + y - yi) * buffer->width + (x + xi)] = color;
+            }
+        }
+    }
+}
 
 uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b)
 {
