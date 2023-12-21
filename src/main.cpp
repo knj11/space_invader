caa0444ef0aa99b0bbb6187f8ba8b221a6a4a8f3
Koby Johnson <koby.johnson@powertraincontrol.com>
2023-12-21 14:02:53 -0500

    defining Alien, Player, Game, SpriteAnimation structs

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,35 @@ struct Sprite
     uint8_t* data;
 };
 
+struct Alien
+{
+    size_t x, y;
+    uint8_t type;
+};
+
+struct Player
+{
+    size_t x, y;
+    size_t life;
+};
+
+struct Game
+{
+    size_t width, height;
+    size_t num_aliens;
+    Alien* aliens;
+    Player player;
+};
+
+struct SpriteAnimation
+{
+    bool loop;
+    size_t num_frames;
+    size_t frame_duration;
+    size_t time;
+    Sprite** frames;
+};
+
 void buffer_clear(Buffer* buffer, uint32_t color)
 {
     for(size_t i = 0; i < buffer->width * buffer->height; ++i)
