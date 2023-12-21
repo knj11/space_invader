de6761d57148a2d003648766c3a1d041c54f851b
Koby Johnson <koby.johnson@powertraincontrol.com>
2023-12-21 12:03:29 -0500

    logging: adding validate shader/program funcs to log any errors

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,32 @@ inline void gl_debug(const char *file, int line) {
 
 #undef GL_ERROR_CASE
 
+void validate_shader(GLuint shader, const char *file = 0){
+    static const unsigned int BUFFER_SIZE = 512;
+    char buffer[BUFFER_SIZE];
+    GLsizei length = 0;
+
+    glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
+
+    if(length>0){
+        printf("Shader %d(%s) compile error: %s\n", shader, (file? file: ""), buffer);
+    }
+}
+
+bool validate_program(GLuint program){
+    static const GLsizei BUFFER_SIZE = 512;
+    GLchar buffer[BUFFER_SIZE];
+    GLsizei length = 0;
+
+    glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
+
+    if(length>0){
+        printf("Program %d link error: %s\n", program, buffer);
+        return false;
+    }
+
+    return true;
+}
 
 struct Buffer
 {
